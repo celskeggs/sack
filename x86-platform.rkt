83d3bc4e4120f7ebaf7189c93d26db72cc727740
Colby Skeggs <skeggsc@catlin.edu>
2014-11-24 21:07:28 -0800

    More work on instruction specification.

--- x86-platform.rkt
+++ x86-platform.rkt
@@ -19,58 +19,44 @@
                                 (definition-syntax "global " name "\n" name ":")
                                 (jumptarget-syntax ".lab" name ":")
                                 (instructions
-                                 (inst (movfm (register dest) (register source))
+                                 (inst (x86/movfm/c (register dest) (constant source))
                                        ("  mov " dest ", [" source "]")
                                        (behavior
-                                        (set-register dest (get-memory source)))
-                                       (stack
-                                        (push (get-memory (pop))))
-                                       )
-                                 (inst (add dest source)
+                                        (set-reg dest (x86/get-memory/c (unconst source))))
+                                       (yields
+                                        (rule
+                                         (source const?)
+                                         (x86/get-memory/c source)
+                                         (x86/movfm/c source))))
+                                 (inst (x86/add/dc (register dest) (register source))
                                        ("  add " dest ", " source)
                                        (behavior
-                                        (set-register dest (+ (get-register source) (get-register dest))))
-                                       (stack
-                                        (push (+ (pop) (pop))))
-                                       (substitute
-                                        
-                                        )
-                                       )
-                                 (inst (mov (register dest) (register source))
+                                        (set-reg dest (+ (get-reg dest) (get-reg source))))
+                                       (yields
+                                        (rule
+                                         (source any?) (dest any?)
+                                         (+ dest source)
+                                         (x86/add/dc dest source))))
+                                 (inst (x86/mov/d (register dest) (register source))
                                        ("  mov " dest ", " source)
                                        (behavior
-                                        (set-register dest (get-register source)))
-                                       (stack
-                                        (push (pop)))
-                                       )
-                                 (inst (movcst (register dest) (constant source))
+                                        (set-reg dest (get-reg source)))
+                                       (yields
+                                        (rule
+                                         (source any?)
+                                         source
+                                         (x86/mov/d source)
+                                        ))) ; would yield a nullary rule.
+                                 (inst (x86/mov/c (register dest) (constant source))
                                        ("  mov " dest ", " source)
                                        (behavior
-                                        (set-register dest source))
-                                       (stack
-                                        (push source)))
+                                        (set-reg dest (unconst source)))
+                                       (yields
+                                        (rule
+                                         (source const?)
+                                         source
+                                         (x86/mov/c (unconst source)))))
                                  )))
-
-(define x86-reduction '(reduction-behavior
-                        (replace (* (number a) (number b))
-                                 (lambda (a b) (* a b)))
-                        (replace (+ (number a) (number b))
-                                 (lambda (a b) (+ a b)))))
-
-(define reduced '((return (+ (arg 0) (arg 1)))
-                  (set-register eax (+ (arg 0) (arg 1)))
-                  (set-register eax (+ (get-memory (+ (get-register ebp) (+ 8 (* 0 4)))) (arg 1)))
-                  (set-register eax (+ (get-memory (+ (get-register ebp) (+ 8 0))) (arg 1)))
-                  (set-register eax (+ (get-memory (+ (get-register ebp) 8)) (get-memory (+ (get-register ebp) (+ 8 (* 1 4))))))
-                  (set-register eax (+ (get-memory (+ (get-register ebp) 8)) (get-memory (+ (get-register ebp) (+ 8 4)))))
-                  (set-register eax (+ (get-memory (+ (get-register ebp) 8)) (get-memory (+ (get-register ebp) 12))))
-                  (set-register eax (+ (get-memory (+ (get-register ebp) 8)) (get-memory (+ (get-register ebp) 12))))
-                  (push (+ (get-memory (+ (pop) 8)) (get-memory (+ (pop) 12))))
-                  (push (+ (get-memory (+ (pop) (movcst 8))) (get-memory (+ (pop) (movcst 12)))))
-                  (push (+ (get-memory (add (pop) (movcst 8))) (get-memory (add (pop) (movcst 12)))))
-                  (push (+ (movfm (add (pop) (movcst 8))) (movfm (add (pop) (movcst 12)))))
-                  (push (add (movfm (add (pop) (movcst 8))) (movfm (add (pop) (movcst 12)))))
-                  ))
 
 (define (has-boxes expr)
   (or (box? expr) (and (pair? expr) (or (has-boxes (car expr)) (has-boxes (cdr expr))))))
@@ -189,47 +175,6 @@
         #t)
       #f))
 
-(define reduced2 '(((swr 0 (arg 0))
-                    (swr 1 (arg 1))
-                    (swr 2 (+ (swr 0) (swr 1)))
-                    (return (swr 2)))
-                   ((swr 0 (get-memory (+ (reg ebp) (+ 8 (* 0 4)))))
-                    (swr 1 (get-memory (+ (reg ebp) (+ 8 (* 1 4)))))
-                    (swr 2 (+ (swr 0) (swr 1)))
-                    (return (swr 2)))
-                   ((swr 3 (+ (reg ebp) (+ 8 (* 0 4))))
-                    (swr 0 (get-memory (swr 3)))
-                    (swr 1 (get-memory (+ (reg ebp) (+ 8 (* 1 4)))))
-                    (swr 2 (+ (swr 0) (swr 1)))
-                    (return (swr 2)))
-                   ((swr 6 (* (cst 0) (cst 4)))
-                    (swr 5 (+ (cst 8) (swr 6)))
-                    (swr 4 (reg ebp))
-                    (swr 3 (+ (swr 4) (swr 5)))
-                    (swr 0 (get-memory (swr 3)))
-                    (swr 7 (* (cst 1) (cst 4)))
-                    (swr 8 (+ (cst 8) (swr 7)))
-                    (swr 9 (+ (swr 4) (swr 8)))
-                    (swr 1 (get-memory (swr 9)))
-                    (swr 2 (+ (swr 0) (swr 1)))
-                    (reg eax (swr 2)))
-                   ((14 (cst 0))
-                    (12 (cst 4))
-                    (6 (* (14) (12)))
-                    (13 (cst 8))
-                    (5 (+ (13) (6)))
-                    (4 (ebp))
-                    (3 (+ (4) (5)))
-                    (0 (get-memory (3)))
-                    (11 (cst 1))
-                    (7 (* (11) (12)))
-                    (10 (cst 8))
-                    (8 (+ (10) (7)))
-                    (9 (+ (4) (8)))
-                    (1 (get-memory (9)))
-                    (2 (+ (0) (1)))
-                    (eax (2)))))
-
 (define-syntax-rule (boxdag-rule (arg cond) ... find repl)
   (list (list (cons 'arg cond) ...) 'find 'repl))
 (define-syntax boxdag-rule-simple
@@ -267,8 +212,8 @@
 
 (define bd (make-boxdag '(+ (arg 0) (const 10))))
 (boxdag-rule-variant x86/2op-comu
-                     (/dc ((a any?) (b const?)) (a b))
-                     (/dc ((a const?) (b any?)) (b a))
+                     (/dc ((a any?) (b const?)) (a (unconst b)))
+                     (/dc ((a const?) (b any?)) ((unconst b) a))
                      (/dm ((a any?) (b mem?)) (a b))
                      (/dm ((a mem?) (b any?)) (b a))
                      (/dd ((a any?) (b any?)) (a b)))
@@ -283,10 +228,10 @@
     (boxdag-rule-simple (arg (argnum number?)) (arg-ref (const argnum)))
     (boxdag-rule-calc (+ (a const?) (b const?)) (const (+ (unconst a) (unconst b))))
     (boxdag-rule-calc (* (a const?) (b const?)) (const (* (unconst a) (unconst b))))
-    (boxdag-rule (a any?) (b const?) (get-memory (+ a b)) (x86/get-memory/dc a b))
-    (boxdag-rule (a const?) (b any?) (get-memory (+ a b)) (x86/get-memory/dc b a))
-    (boxdag-rule (a any?) (b any?) (get-memory (+ a b)) (x86/get-memory/dd a b)))
-   ;(boxdag-rule-calc (unconst (x const?)) (unconst x))
+    (boxdag-rule (a any?) (b const?) (get-memory (+ a b)) (x86/get-memory/dc a (unconst b)))
+    (boxdag-rule (a const?) (b any?) (get-memory (+ a b)) (x86/get-memory/dc b (unconst a)))
+    (boxdag-rule (a any?) (b any?) (get-memory (+ a b)) (x86/get-memory/dd a b))
+    (boxdag-rule-calc (unconst (x const?)) (unconst x)))
    ; WORKING ON MEMORY REFERENCES...
    ; instr/dd, instr/dc, instr/cm, instr/cc, instr/mc, etc. d: dynamic, c: constant, m: memory
    (x86/2op-comu + x86/add)
@@ -301,3 +246,20 @@
 (apply-boxdag-rules-all bd-rules (get-boxdag-map-box bd))
 ;bd
 (strip-boxes (get-boxdag-content bd))
+
+(define-syntax-rule (instruction (name (arg type) ...)
+                                 output-info
+                                 behavior)
+  (list 'name (cons 'arg type) ...
+        'output-info
+        (boxdag-rule (arg type) ...
+                     stripped-behavior
+                     (name arg ...))))
+(define temp (instruction (x86/add/dc (dest any?) (source const?))
+                           ("  add " dest ", " source)
+                           (set-reg dest (+ (get-reg dest) (unconst source)))))
+;                           (yields
+;                            (rule
+;                             (source any?) (dest any?)
+;                             (+ dest source)
+;                             (x86/add/dc dest source)))
